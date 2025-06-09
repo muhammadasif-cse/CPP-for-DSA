6790967521bd6eb8d59df65b4b67f17998c2555b
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-06-09 21:22:10 +0600

    [ADD] - add string constructor examples and main functionality in string_constructor.cpp

---
+++ string_constructor.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string s = "Hello";
+
+    // step 1 string constructor
+    // string s("Hello");
+
+    // step 2 string constructor
+    // string s("Hello World", 5);
+
+    // step 3 string constructor
+    // string s = "Hello World";
+
+    // first 4 characters remove from the string
+    // string t(s, 0, 4); // from index 0 to index 4 (exclusive)
+    // string t(s, 4);    // from index 4 to end
+    // cout << t << endl;
+
+    // step 4
+    string s(5, 'a');  // create a string of 5 'a' characters
+    cout << s << endl; // Output: aaaaa
+
+    return 0;
+}
