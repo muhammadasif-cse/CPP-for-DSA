3fd34a2fa759daac1c7fd46c8668ea4fbf0c8510
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-06-13 00:06:14 +0600

    add sort_array_of_objects.cpp to implement functionality for sorting student data by marks

---
+++ sort_array_of_objects.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+};
+
+bool cmp(Student l, Student r)
+{
+    if (l.marks < r.marks)
+        return true;
+    else
+        return false;
+}
+
+int main()
+{
+
+    int n;
+    cin >> n;
+
+    Student students[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> students[i].name >> students[i].roll >> students[i].marks;
+    }
+
+    // custom compare function
+    sort(students, students + n, cmp);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << students[i].name << " " << students[i].roll << " " << students[i].marks << endl;
+    }
+    return 0;
+}
