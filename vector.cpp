75956e6e0e391361da7641233d36521874aa9c38
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-06-23 22:43:06 +0600

    add vector.cpp to demonstrate basic vector operations and output the last element

---
+++ vector.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v = {10, 20, 30, 40};
+    // v.resize(2);
+    // v.reserve(4);
+    // v.pop_back();
+    // v.insert(v.begin() + 3, 20);
+    // v.erase(v.begin() + 2, v.end());
+
+    cout << v[v.size() - 1];
+
+    // for (auto value : v)
+    // {
+    //     cout << value << " ";
+    // }
+    return 0;
+}
