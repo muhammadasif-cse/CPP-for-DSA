43cd0bb43f8b28922643aceaafedf2e3a34ab6a7
muhammadasif-cse <muhammadasif.cse@gmail.com>
2025-07-04 22:18:10 +0600

    add input-a-linked-list.cpp to implement linked list operations and user input handling

---
+++ input-a-linked-list.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+    Node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
+    }
+};
+
+void insert_tail(Node *&head, Node *&tail, int val)
+{
+    Node *newNode = new Node(val);
+    Node *tmp = head;
+
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void print_value(Node *head)
+{
+    Node *tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << endl;
+        tmp = tmp->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+    while (true)
+    {
+        int val;
+        cin >> val;
+
+        if (val == -1)
+        {
+            break;
+        }
+        insert_tail(head, tail, val);
+    }
+    print_value(head);
+
+    return 0;
+}
